81cf423845d726f6ccd1c12e77467693ef1aa3b8
Malin Hanak <malinehanak@gmail.com>
2021-08-19 17:38:31 +0200

    common words array

---
+++ src/components/file-loader/commonWords.ts
@@ -0,0 +1,9 @@
+const common: string =
+  "a,about,all,an,and,are,as,at,any,be,been,but,by,can,de,den,det,did,do," +
+  "du,for,from,had,has,have,he,her,him,his,i,if,in,is,it,jag,many,me,must,my," +
+  "no,not,of,on,once,or,out,s,said,she,so,that,the,their,there,them,then,this,till," +
+  "to,var,was,went,were,what,when,where,which,while,who,whom,will,with,would,you,your,är";
+
+export const commonWordArray = () => {
+  return common.split(",");
+};
