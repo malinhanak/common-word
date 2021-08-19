201b231cd62ab91887ee51d8b2396f6af3cec956
Malin Hanak <malinehanak@gmail.com>
2021-08-19 17:37:42 +0200

    Styles added to Global

--- src/theme/GlobalStyle.tsx
+++ src/theme/GlobalStyle.tsx
@@ -3,5 +3,14 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${(props) => props.theme.colors.main};
+    font-size: 16px;
+    font-family: "Poppins";
+    font-weight: 200;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 `;
