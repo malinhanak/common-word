df4d2a3d79bb931309c3ced9c925e3c01b8ec956
Malin Hanak <malinehanak@gmail.com>
2021-08-19 17:38:02 +0200

    Pagetitle and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,7 @@ interface HeaderProps {
 const Header: FunctionComponent<HeaderProps> = ({ theme }) => {
   return (
     <Style>
+      <PageTitle>Vilket är det vanligaste order?</PageTitle>
       <Icon>
         <ModeToggle>
           <ToggleLight />
@@ -24,8 +25,16 @@ export default withTheme(Header);
 const Style = styled.header`
   display: flex;
   justify-content: space-between;
+  align-self: stretch;
 `;
 
 const Icon = styled.div`
-  margin: 1rem;
+  margin: 1rem 1.5rem;
+  cursor: pointer;
+`;
+
+const PageTitle = styled.h1`
+  margin: 0 0 0 1.5rem;
+  font-size: 1.5rem;
+  color: ${(props) => props.theme.colors.secondary};
 `;
